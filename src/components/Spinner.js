4dba8d0021b4958a1a5b396ff6a74d6c9512e603
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2024-12-28 18:19:37 +0200

    implement getCardFromSelector

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,7 +2,7 @@ import { rewards } from "../data/rewards";
 import { Card } from "./Card";
 import './Spinner.css';
 import { useAnimate, motion, cubicBezier } from "motion/react";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function Spinner() {
     const rouletteRewards = rewards;
@@ -12,6 +12,28 @@ function Spinner() {
 
     const [scope, animate] = useAnimate();
     const [startPosition, setStartPosition] = useState(0);
+    const selectorRef = useRef(null);
+
+    // Find the card selector is pointing to
+    const getCardFromSelector = () => {
+        if (selectorRef.current) {
+            const selectorRect = selectorRef.current.getBoundingClientRect();
+            const selectorLeft = parseFloat(selectorRect.left);
+            const selectorWidth = parseFloat(selectorRect.width);
+            const selectorCenter = selectorLeft + selectorWidth / 2;
+            // Convert HTMLCollection to an array for easy iteration
+            const cards = Array.from(document.getElementsByClassName('card'));
+            // Find the card that the selector is pointing to
+            const cardUnderSelector = cards.find(card => {
+                const cardPosition = card.getBoundingClientRect();
+                const cardLeft = parseFloat(cardPosition.left);
+                const cardRight = parseFloat(cardPosition.right);
+                return selectorCenter >= cardLeft && selectorCenter <= cardRight;
+            });
+            return cardUnderSelector;
+        }
+        return null;
+    };
 
     // Normalize distance
     useEffect(() => {
@@ -27,7 +49,7 @@ function Spinner() {
 
     const renderRows = () => {
         const rows = []
-        const rowContent = rouletteRewards.map((r, idx) => <Card key={idx} rewardStr={r} />);
+        const rowContent = rouletteRewards.map((r, idx) => <Card key={idx} className="$card" rewardStr={r} />);
         const row = <div className="row">{rowContent}</div>;
         for (let i = 0; i < 29; i++) {
             rows.push(row);
@@ -44,6 +66,7 @@ function Spinner() {
 
     const handleSpin = () => {
         const newLandingPosition = calculateLandingPosition();
+        console.log(getCardFromSelector().querySelector('.card-content').innerText);
         animate(scope.current, {
             x: startPosition + newLandingPosition,
             transition: {
@@ -60,7 +83,7 @@ function Spinner() {
                 <h1>DUMMY SPINNER</h1>
             </div>
             <div className="roulette-wrapper">
-                <div className="selector"></div>
+                <div ref={selectorRef} className="selector"></div>
                 <motion.div
                     className="wheel"
                     ref={scope}
